8b3495f308c562b3fe8cb2908c7b7125da95ad4b
Long-as-Python <45416328+Long-as-Python@users.noreply.github.com>
2019-11-23 16:28:32 +0200

    general improvements

--- Sky Danger/Assets/scripts/shipMovement.cs
+++ Sky Danger/Assets/scripts/shipMovement.cs
@@ -8,13 +8,25 @@ public class shipMovement : MonoBehaviour
 
     public float speed = 12f;
 
+    public float mouseSense = 100f;
+
+    public Transform playerbody;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSense * Time.deltaTime;
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        playerbody.Rotate(Vector3.up * mouseX);
 
         Vector3 move = transform.right * x + transform.forward * z;
+        
 
         controller.Move(move * speed * Time.deltaTime);
     }
