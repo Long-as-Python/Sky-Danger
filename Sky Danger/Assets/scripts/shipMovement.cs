1aef75b4beedcabb44aeab10e449072a97adc0fa
Long-as-Python <45416328+Long-as-Python@users.noreply.github.com>
2019-11-23 15:24:22 +0200

    bug deleted

--- Sky Danger/Assets/scripts/shipMovement.cs
+++ Sky Danger/Assets/scripts/shipMovement.cs
@@ -2,22 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpaceShipSc : MonoBehaviour
+public class shipMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        public float speed;
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            
-        }
+        
     }
 }
