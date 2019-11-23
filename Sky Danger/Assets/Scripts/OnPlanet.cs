a0c164e1f2ab5ee6b6bdbe958a5bc90a8d7180a8
justpisun <56193422+justpisun@users.noreply.github.com>
2019-11-23 16:38:13 +0200

    add planet and ontriggerenter with planet
    add planet and ontriggerenter with planet

---
+++ Sky Danger/Assets/Scripts/OnPlanet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnPlanet : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag) {
+            case "ForestPlanet":
+                Application.LoadLevel("ForestPlanet");
+                Debug.Log("Enter ForestPlanet");
+                break;
+        }
+    }
+}
